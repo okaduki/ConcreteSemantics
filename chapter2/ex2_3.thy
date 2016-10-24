55cf9169c3f2426526315e509072df87e929ed8a
okaduki <okaduki1@gmail.com>
2016-10-24 21:43:20 +0900

    add solution of exercise 2.3

---
+++ chapter2/ex2_3.thy
@@ -0,0 +1,15 @@
+theory ex2_3 imports Main begin
+
+fun count :: "'a \<Rightarrow> 'a list \<Rightarrow> nat" where
+"count y Nil = 0" |
+"count y (x#xs) = (if x = y then 1+(count y xs) else count y xs)"
+
+(* I cannot understand why the value is not 1*)
+value "count 1 (1#2#3#[])"
+
+theorem "count x xs \<le> length xs"
+apply(induction xs)
+apply auto
+done
+
+end
