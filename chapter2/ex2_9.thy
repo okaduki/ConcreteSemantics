db5fc25360ad5385f2a3cb456758a95f2dbd35d9
okaduki <okaduki1@gmail.com>
2016-10-24 22:55:32 +0900

    add solution of exercise 2.9

---
+++ chapter2/ex2_9.thy
@@ -0,0 +1,22 @@
+theory ex2_9 imports Main begin
+
+fun itadd :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+"itadd 0 n = n" |
+"itadd (Suc m) n = itadd m (Suc n)"
+
+(** see section 2.2.2 **)
+fun add :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+"add 0 n = n" |
+"add (Suc m) n = Suc (add m n)"
+
+lemma add_r[simp]: "add m (Suc n) = Suc (add m n)"
+apply(induction m)
+apply auto
+done
+
+theorem "itadd m n = add m n"
+apply(induction m arbitrary: n)
+apply auto
+done
+
+end
